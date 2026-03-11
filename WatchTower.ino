61d48ecf2c3d3bea8172b49074125dd5eb930708
Mike Burton <mb-github@niskala.org>
2026-03-11 08:10:19 -0700

    nit

--- WatchTower.ino
+++ WatchTower.ino
@@ -10,7 +10,7 @@
 //     - WiFiManager ~2.0.17
 //     - ArduinoMDNS ~1.0.0
 // - set the PIN_ANTENNA to desired output pin
-// - set the timezone as desired
+// - set the TIMEZONE as desired
 // - build and run the code on your device
 // - connect your phone to "WatchTower" to set the wifi config for the device
 // - connect to http://watchtower.local to view current status
